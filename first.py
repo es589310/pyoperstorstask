37f9da8c494a26150cf6477978a699c32ee7d10f
Emin Panahov <panahovemin10@gmail.com>
2022-12-01 23:05:34 +0400

    ders tapsiriqi yerine yetirildi first.py

---
+++ first.py
@@ -0,0 +1,23 @@
+# istifadəçinin adını tələb edin
+# istifadəçidən yaşını tələb edin
+# istifadəçinin iki övladı olduğunu qəbul edək hər bir övladının yaşını tələb edin
+# istifadəçinin yaşı ilə övladlarının yaşlarınn ortalaması arasındakı fərqi ekrana çap edin
+# Ekranda Ehmed adlı istifadəçinin övladlarının yaş ortalası ilə Ehmedin yaşı arasındakı fərq 12-dir tipli bir mesaj görünməlidir
+# Burda Ehmed istifadəçinin daxil etdiyi addir
+
+
+
+a=input("Username: ")
+print(a=="Enes")
+
+b=input("age: ")
+c=input("thefirstchild: ")
+d=input("thesecondchild: ")
+print(b=="40" and c=="12" and d=="10")
+
+e=input("Enes adlı istifadəçinin övladlarının yaş ortalası ilə Enesin yaşı arasındakı fərq 29-dır ")
+print(e)
+
+
+
+
